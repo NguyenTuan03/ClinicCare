b7b0f4f6d6521c448df9deb6cabb4f1f2e0d3ed7
NguyenTuan03 <nguyenanhtuan.170164@gmail.com>
2026-06-24 22:59:51 +0700

    refactor: rename schedule to appointment and update param requirements in appointments controller

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -4,62 +4,62 @@ module Api
       before_action :authorize_request
 
       def index
-        schedule = Appointment.all
-        render_success(data: schedule, message: "Success", status: :ok)
+        appointment = Appointment.all
+        render_success(data: appointment, message: "Success", status: :ok)
       end
 
       def show
-        schedule = Appointment.find_by(id: params[:id])
-        if !schedule
-          return render_error(message: "Schedule not found", status: :not_found)
+        appointment = Appointment.find_by(id: params[:id])
+        if !appointment
+          return render_error(message: "Appointment not found", status: :not_found)
         end
-        render_success(data: schedule, message: "Success", status: :ok)
+        render_success(data: appointment, message: "Success", status: :ok)
       end
 
       def create
-        schedule = Appointment.new(schedule_params)
+        appointment = Appointment.new(appointment_params)
         puts "current user", @current_user
         # Only doctor can create schedule
         if @current_user.role.name != "doctor"
-          return render_error(message: "You don't have permission to create schedule", status: :unauthorized)
+          return render_error(message: "You don't have permission to create appointment", status: :unauthorized)
         end
 
-        schedule.user_id = @current_user.id
+        appointment.user_id = @current_user.id
 
-        if schedule.save
-          render_success(data: schedule, message: "Success", status: :created)
+        if appointment.save
+          render_success(data: appointment, message: "Success", status: :created)
         else
-          render_error(message: schedule.errors.full_messages, status: :unprocessable_entity)
+          render_error(message: appointment.errors.full_messages, status: :unprocessable_entity)
         end
       end
 
       def update
-        schedule = Schedule.find_by(id: params[:id])
-        if !schedule
-          return render_error(message: "Schedule not found", status: :not_found)
+        appointment = Appointment.find_by(id: params[:id])
+        if !appointment
+          return render_error(message: "Appointment not found", status: :not_found)
         end
 
-        if schedule.update(schedule_params)
-          render_success(data: schedule, message: "Success", status: :ok)
+        if appointment.update(appointment_params)
+          render_success(data: appointment, message: "Success", status: :ok)
         else
-          render_error(data: schedule.errors, message: "Failed", status: :unprocessable_entity)
+          render_error(data: appointment.errors, message: "Failed", status: :unprocessable_entity)
         end
       end
 
       def destroy
-        schedule = Schedule.find_by(id: params[:id])
-        if !schedule
-          return render_error(message: "Schedule not found", status: :not_found)
+        appointment = Appointment.find_by(id: params[:id])
+        if !appointment
+          return render_error(message: "Appointment not found", status: :not_found)
         end
 
-        schedule.destroy
-        render_success(data: schedule, message: "Success", status: :ok)
+        appointment.destroy
+        render_success(data: appointment, message: "Success", status: :ok)
       end
 
       private
 
-      def schedule_params
-        params.require(:schedule).permit(:date, :start_time, :end_time)
+      def appointment_params
+        params.require(:appointment).permit(:date, :start_time, :end_time, :user_id)
       end
     end
   end
